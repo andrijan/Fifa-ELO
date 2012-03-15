8cb3227c83028d4be8cfa8c99cdc93c9696a0d44
Andri Janusson <andrijan@gmail.com>
2012-03-15 10:40:31 +0000

    Added AchievementsAdmin

--- core/admin.py
+++ core/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 
-from models import *
+from models import Achievement, Player, Team, Game, Points
 
-admin.site.register(Achievement)
+class AchievementAdmin(admin.ModelAdmin):
+    list_display = ('name', 'points')
+
+admin.site.register(Achievement, AchievementAdmin)
 admin.site.register(Player)
 admin.site.register(Team)
 admin.site.register(Game)
