ee96e1f5fb89f96d6b8e08b6b2b860bc45eccd61
Andri Janusson <andrijan@gmail.com>
2012-03-19 12:02:48 +0000

    Admin changes

--- core/admin.py
+++ core/admin.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
 
-from models import Achievement, Player, Team, Game, Points
+from models import Achievement, Player, Team, Game, Points, FifaTeam
 
 class AchievementAdmin(admin.ModelAdmin):
     list_display = ('name', 'points')
 
+class GameAdmin(admin.ModelAdmin):
+    list_display = ('home_team', 'home_fifa_team', 'away_fifa_team', 'away_team', 'result', 'home_score', 'away_score')
+
+admin.site.register(FifaTeam)
 admin.site.register(Achievement, AchievementAdmin)
 admin.site.register(Player)
 admin.site.register(Team)
